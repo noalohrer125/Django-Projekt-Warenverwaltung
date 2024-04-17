6028665e3615a52198015d12c465a45496106093
noaloehrer <noa.loehrer@optimatik.ch>
2024-04-17 08:52:32 +0200

    c9_style_anpassungen_und_login_form

--- collectionmanager/views.py
+++ collectionmanager/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 def home(request):
     return render(request, 'Home.html')
 
 def mycollection(request):
-    return render(request, 'MyCollection.html')
+    if request.user.is_authenticated:
+        return render(request, 'mycollection.html')
+    else:
+        return redirect('http://127.0.0.1:8000/login/')
 
 def login(request):
     return render(request, 'login.html')
